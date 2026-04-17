75838ca54f45cf3a3a7373d7836de752cde8c68c
Sanesh Kumar <162696738+Sanesh764@users.noreply.github.com>
2026-04-17 18:20:56 +0530

    Add files via upload

---
+++ varible.js
@@ -0,0 +1,18 @@
+
+let a="sanesh";
+var b="papi";
+c="1234";
+const d=1224;
+let f=null;
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+
+console.table([a,b,c,d,f]);
+/*
+A variable is a named memory location that holds a value.
+why we use 
+to store data,to reuse data
+to make code flixible
+To perform operations
+*/ 
