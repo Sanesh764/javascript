46f8ab326eaeaccda02acab0258d3a8d4855dcca
unknown <sanesh7644@.com>
2026-04-18 08:07:20 +0530

    varible

---
+++ basic/nums_math.js
@@ -0,0 +1,33 @@
+const score=600
+// console.log(score);
+
+const balance =new Number(100);
+// console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));///decimal ke badh kitna point dekhega
+
+// const otherNumber=23.8966;//output=23.9
+// const otherNumber=123.8966;//output 124
+const otherNumber=1123.8966;//output =1.12e+3
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundred=100000000000
+// console.log(hundred.toLocaleString('en-IN'));//ye cooma karke print karega
+
+
+
+
+
+//=================math++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4,6));
+// console.log(Math.min(-4,2));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.8));
+(
+console.log((Math.random()*10)+1)
+
+
---
+++ basic/string.js
@@ -0,0 +1,28 @@
+
+// const name=new String("papumadharchod");
+// console.log(name);
+// console.log(name[2]);
+// console.log(name.__proto__);
+// console.log(name.toUpperCase());
+// console.log(name.indexOf("u"))
+// console.log(name.charAt(2));
+// const newNmae=name.substring(0,4);
+// console.log(newNmae);//isme 4 include nhi hua 3 tak hi gaya
+
+// const anotherString=name.slice(-8,4);
+// console.log(anotherString)
+
+const newStringOne="       sanesh      ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https://sanesh.com/sanesh%20kumar"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('sansh'));
+
+
+
+
+
