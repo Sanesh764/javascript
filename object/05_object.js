868f2f2f4c7a0036394608e60f3dbf1863c8e36d
Sanesh <sanesh7644@gmail.com>
2026-04-19 11:31:02 +0530

    json

---
+++ object/05_object.js
@@ -0,0 +1,42 @@
+
+const course={
+    courseName:"js in hindi",
+    price:234,
+    couseInstructre:"histesh sir"
+}
+// course.couseInstructre
+
+const {couseInstructre:instructre
+}=course;//name change kar diya instruectre kar diye isko hi destructuring bolte h
+console.log(instructre);
+
+// const navbar=({company})=>{//props
+//     console.log(company);
+// }
+//navbar(company="hitest");//yaha navbar ko call kiye hai and value aasign kiye
+
+
+
+//+++++++++++++++++++++++API++++++++++++++++++++++++++++++++
+
+//json formet
+// {
+//     "Name":"sanesh",
+//     "course":"js in hindi",
+//     "price":"free"
+// }
+
+//jso formet array ke roop me bhi milta hai
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
+
+
+
