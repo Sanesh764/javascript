1fc958c7872319adaeb0a8414ccf18444f214fd1
Sanesh <sanesh7644@gmail.com>
2026-04-28 20:57:01 +0530

    prototype how work in javascript

---
+++ Oops/prototype.js
@@ -0,0 +1,26 @@
+
+// let myName="sanesh      ";
+// console.log(myName.trueLenght);
+
+let myheroes=["thor","spiderman"];
+
+let heroPower={
+    thor:"hammer",
+    spiderman:"sling",
+
+    getSpiderPower:function(){
+        console.log(`slidy power is ${this.spiderman}`);
+        
+    }
+}
+Object.prototype.sanesh=function(){
+    console.log("your are the best in all function we access");
+    
+}
+Array.prototype.mysanesh=function(){
+    console.log("hey sanesh what are you do");
+    
+}
+// heroPower.sanesh();
+myheroes.sanesh();
+// heroPower.mysanesh();//give error
