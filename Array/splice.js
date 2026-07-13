6d3ebac690419aa4e6eb7c24ef7ee7b4a5cc41c9
sanesh <sanesh7644@gmail.com>
2026-07-13 07:12:31 +0530

    add examples for inserting multiple elements using splice method

--- Array/splice.js
+++ Array/splice.js
@@ -24,6 +24,10 @@ console.log(fruits);
 fruits.splice(1,1,"tamatar");//index 1 pr jao 1 element delete karo or uske jagah pr new element add karo tamatar
 console.log(fruits);
 
+fruits.splice(1,0,"licchi","pumkin","sushi");
+console.log(fruits);
+
+
 
 
 
