64f4960f7940512f5b7fb0f07b8131cf4b376f41
unknown <sanesh7644@gmail.com>
2026-07-13 07:08:49 +0530

    refactor splice.js to clean up comments and demonstrate element replacement

--- Array/splice.js
+++ Array/splice.js
@@ -9,8 +9,23 @@ const fruits = [
 
 console.log(fruits);
 
-const result=fruits.slice(1,3);
-console.log(result);
-result.splice(1,0,"licchi");
-console.log(result);
+//delete the element 
+// const result=fruits.splice(1,2);//ye hamisa delete hua element return karta hai
+// console.log(result);
+
+// console.log(fruits);
+
+
+//insert element
+//fruits.splice(1,0,"licchi");//first index pr jao 0 element delete karo licchi add kar do index 1 pr
+//console.log(fruits);
+
+//replace element
+fruits.splice(1,1,"tamatar");//index 1 pr jao 1 element delete karo or uske jagah pr new element add karo tamatar
+console.log(fruits);
+
+
+
+
+
 
