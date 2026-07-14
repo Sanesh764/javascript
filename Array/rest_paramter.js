71ab0a230e48cb514f7f94985f12c715643dce14
Sanesh <sanesh7644@gmail.com>
2026-07-14 07:07:29 +0530

    add index2.html and server.js for basic DOM manipulation example

--- Array/rest_paramter.js
+++ Array/rest_paramter.js
@@ -29,3 +29,5 @@ AddToCart(
     "headphones"
 );
 
+
+
---
+++ DOM/server.js
@@ -0,0 +1,11 @@
+
+const heading=document.getElementById("title");
+console.log(heading);
+
+// const para=document.getElementsByClassName("text");
+// console.log(para);
+
+const para=document.querySelector(".text");
+console.log(para);
+
+
