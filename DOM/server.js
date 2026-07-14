0986008fa7c9355b692d584b1194e47911e57e44
Sanesh <sanesh7644@gmail.com>
2026-07-14 10:52:44 +0530

    add event listeners for button click and form submission in server.js

--- DOM/server.js
+++ DOM/server.js
@@ -25,3 +25,24 @@ p.forEach((ele)=>{
     ele.innerHTML="this is using quesryselectorAll method";
 })
 
+// const box=document.querySelector(".box");
+// box.classList.add("active");
+
+const h2=document.createElement("h2");
+h2.innerText="aldi waha say hato";
+document.body.appendChild(h1);
+
+const btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+    const p=document.createElement("p");
+    p.innerText="new paragraph is added";
+    document.body.appendChild(p);
+});
+
+const form=document.querySelector("form");
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("form was submitted");
+    
+})
+
