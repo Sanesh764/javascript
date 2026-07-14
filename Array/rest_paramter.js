8b67baffb2b71652423f97d2ac6e72a3db8f56cb
Sanesh <sanesh7644@gmail.com>
2026-07-14 06:29:04 +0530

    add rest_paramter.js to implement sum and multiply functions using rest parameters

---
+++ Array/rest_paramter.js
@@ -0,0 +1,18 @@
+
+function sum(...numbers){
+    let totalSum=0;
+    for(let num of numbers){
+        totalSum += num;
+    }
+    return totalSum;
+};
+console.log(sum(10,20));
+console.log(sum(10,20,80,90));
+console.log(sum(10,20,45,87,1,2,3,4,5,89,6,78));
+
+function multiply(multiplyer,...numbers){
+    return numbers.map(numbers=>numbers*multiplyer);
+}
+console.log(multiply(2,1,2,3,4));
+
+
