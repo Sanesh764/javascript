ba4b7255453b275c6cc74ef01239f1a39c186c34
Sanesh <sanesh7644@gmail.com>
2026-07-14 07:37:39 +0530

    use queryselectorAll type of attribute

--- DOM/server.js
+++ DOM/server.js
@@ -1,11 +1,27 @@
 
-const heading=document.getElementById("title");
-console.log(heading);
+// const heading=document.getElementById("title");
+// console.log(heading);
 
-// const para=document.getElementsByClassName("text");
+// // const para=document.getElementsByClassName("text");
+// // console.log(para);
+
+// const para=document.querySelector(".text");
 // console.log(para);
 
-const para=document.querySelector(".text");
-console.log(para);
+// const h1=document.getElementById("title");
+// h1.innerText="kya hal chal h";
+const h1=document.querySelector("#title");
+h1.innerText="hello and welcome to our college"
+h1.style.color="red";
 
+// const p=document.getElementsByClassName("text");
+// p[0].innerText="pyar hai to hai or kya";
+
+// const p = document.querySelector(".text");
+// p.innerText = "pyar hai to hai ya kya";
+
+const p=document.querySelectorAll(".text");
+p.forEach((ele)=>{
+    ele.innerHTML="this is using quesryselectorAll method";
+})
 
