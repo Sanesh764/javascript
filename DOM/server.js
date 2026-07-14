3fae8d204362e4a616b6a16c4657e6591b5a9c32
Sanesh <sanesh7644@gmail.com>
2026-07-14 12:19:11 +0530

    commit sms

--- DOM/server.js
+++ DOM/server.js
@@ -43,6 +43,8 @@ const form=document.querySelector("form");
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     console.log("form was submitted");
+    console.log("thans");
+    
     
 })
 
