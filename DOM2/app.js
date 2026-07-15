f3b578c461248a729597ddc1fbf0673c95362067
Sanesh <sanesh7644@gmail.com>
2026-07-15 06:43:31 +0530

    refactor: update app.js to demonstrate innerHTML usage instead of innerText

--- DOM2/app.js
+++ DOM2/app.js
@@ -7,10 +7,15 @@
 //textContent
 
 // const h1 = document.querySelector("#h1");
-// console.log(h1.textContent);
-// h1.textContent = "<b>Sanesh</b>";
+// console.log(h1.textContent);//
+// h1.textContent = "<b>Sanesh</b>";//yaha hidden word bhi dekh jata h but <b>sanesh</b> ishi hi print hoga
 
 //innerText
+// const h1 = document.querySelector("#h1");
+// console.log(h1.innerText);
+// h1.innerText = "<b>Sanesh</b>";//yaha direct <b>sanesh</b> print hoga no chnage
+
+//innerHtml
 const h1 = document.querySelector("#h1");
-console.log(h1.innerText);
-h1.innerText = "<b>Sanesh</b>";
+console.log(h1.innerHTML);
+h1.innerHTML = "<b>Sanesh</b>";//yaha text ko actual me bold kae dega
