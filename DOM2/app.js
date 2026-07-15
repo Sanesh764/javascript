079f8c2010fc89bc0998521b8892ea4b87746b7a
Sanesh <sanesh7644@gmail.com>
2026-07-15 06:52:18 +0530

    refactor: update app.js to remove innerHTML example and enhance button functionality

--- DOM2/app.js
+++ DOM2/app.js
@@ -16,6 +16,26 @@
 // h1.innerText = "<b>Sanesh</b>";//yaha direct <b>sanesh</b> print hoga no chnage
 
 //innerHtml
-const h1 = document.querySelector("#h1");
-console.log(h1.innerHTML);
-h1.innerHTML = "<b>Sanesh</b>";//yaha text ko actual me bold kae dega
+// const h1 = document.querySelector("#h1");
+// console.log(h1.innerHTML);
+// h1.innerHTML = "<b>Sanesh</b>";//yaha text ko actual me bold kae dega
+
+const p=document.createElement("p");
+p.innerText="loren 5000";
+document.body.appendChild(p);
+
+p.remove();
+
+const old=document.querySelector("h1");
+const h2=document.createElement("h2");
+const button=document.createElement("button");
+button.innerText="click me";
+document.body.appendChild(button);
+
+button.addEventListener("click",()=>{
+    console.log("button was clicked");
+    
+})
+
+h2.innerText="mai hu code karne wla balak";
+old.replaceWith(h2);
