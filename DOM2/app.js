17d06ca80dd5ef9b5d229f7a4c7ee6eec8301f08
Sanesh <sanesh7644@gmail.com>
2026-07-15 06:39:46 +0530

    add initial HTML, CSS, and JavaScript files for DOM manipulation

---
+++ DOM2/app.js
@@ -0,0 +1,11 @@
+
+// const h1=document.querySelector("#h1");
+// //h1.innerText="hello world";
+// //h1.innerHTML="you are good";
+// h1.textContent="kya hal chal";
+
+//textContent
+
+const h1 = document.querySelector("#h1");
+console.log(h1.textContent);
+h1.textContent = "<b>Sanesh</b>";
